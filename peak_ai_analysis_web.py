3d7ebe701f180c6e68b7dbb85415d1f37a95db76
HiroyukiMorimura <161313097+HiroyukiMorimura@users.noreply.github.com>
2025-08-06 00:42:00 +0900

    Update peak_ai_analysis_web.py

--- peak_ai_analysis_web.py
+++ peak_ai_analysis_web.py
@@ -966,14 +966,14 @@ class RamanPDFReportGenerator:
     def setup_japanese_font(self):
         """日本語フォントの設定（フォールバック戦略強化）"""
         self.japanese_font_available = False
-        self.japanese_font_name = 'Times-Roman'  # デフォルトを必ず定義
+        self.japanese_font_name = 'HeiseiMin-W3'  # デフォルトを必ず定義
 
         try:
             # システムにある日本語フォントを探す
             font_paths = [
             # Windows - 日本語フォント
             "C:/Windows/Fonts/yumin.ttf",
-                
+            
             """
             "C:/Windows/Fonts/yumin.ttf",
             "C:/Windows/Fonts/msgothic.ttc",
